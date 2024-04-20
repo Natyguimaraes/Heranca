295c9bc486b898dc14c8ec2631ddd0736765ad31
Natyguimaraes <santosnath433@gmail.com>
2024-04-19 21:14:01 -0300

    f

--- src/funcionario.js
+++ src/funcionario.js
@@ -1,18 +1,28 @@
 import Pessoa from './pessoa.js'
 
 class Funcionario extends Pessoa {
-    constructor(nome, idade, cargo) {
+    constructor(nome, idade, cargo, salario) {
         super(nome, idade);
         this.cargo = cargo;
+        this.salario = salario;
     }
 
-    mostrarDados() {
+    mostrarDadosFunc() {
         super.mostrarDados();
-        console.log(`Sou um ${this.cargo}.`);
+        console.log(`Sou um(a) ${this.cargo}, meu salário é ${this.salario}.`);
+    }
+
+atualizarDados(novoNome, novaIdade, novoCargo, novoSalario){
+    super.atualizarDados(novoNome, novaIdade);
+    this.cargo = novoCargo;
+    this.salario = novoSalario;
     }
 }
 
-const funcionario1 = new Funcionario("Naty", 20,"Motorista");
-funcionario1.mostrarDados();
+const funcionario1 = new Funcionario("Naty", 20,"Programadora", 4000);
+funcionario1.mostrarDadosFunc();
+
+funcionario1.atualizarDados("Natalia Guimaraes", 20, "Programadora", 4120);
+funcionario1.mostrarDadosFunc();
 
 export default Funcionario; 
--- src/pessoa.js
+++ src/pessoa.js
@@ -8,6 +8,15 @@ class Pessoa {
     mostrarDados(){
         console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
     } //instaciamento da classe// 
+
+    atualizarDados(novoNome, novaIdade){
+        this.nome = novoNome;
+        this.idade = novaIdade;  
+    }
 }
+
+//const pessoa1 = new Pessoa("Natália", 20);
+//pessoa1.mostrarDados();
+
 
 export default Pessoa;
